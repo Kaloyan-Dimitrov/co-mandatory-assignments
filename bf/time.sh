e5a8b21466f1170818a376c5a82880438805d944
Mihail Mihov <mihovmihailp@gmail.com>
2024-10-13 17:17:34 +0200

    Add more programs

--- bf/time.sh
+++ bf/time.sh
@@ -5,7 +5,7 @@ LANG=s make
 for f in $(find progs/ -maxdepth 1 -type f -name "*.b"); do
     echo "Timing for ${f}:"
     if [[ -f "${f%.*}.in" ]]; then
-        echo "time bash -c './brainfuck "$1" < "$2" > /dev/null' _ "${f}" "${f%.*}.in""
+        time bash -c './brainfuck "$1" < "$2" > /dev/null' _ "${f}" "${f%.*}.in"
     else
         time bash -c './brainfuck "$1" > /dev/null' _ "${f}"
     fi
